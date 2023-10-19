c0c82d60139a9795ec17f6acbfe8017ddab62dbb
franco ortiz <franco_ortiz_08@hotmail.com>
2023-10-19 17:56:39 -0300

    first commit

---
+++ ejercicios/java.js
@@ -0,0 +1,189 @@
+//1
+//document.write("hola friki ");
+
+//alert("hola FREEKE");
+
+//var edad1;
+//var edad2;
+
+
+//edad1 = prompt("Ingrese su edad");
+//edad2 = prompt("Ingrese su edad");
+//suma = parseInt(edad1)  + parseInt(edad2);
+
+
+//document.write("la suma de las edades es:" + suma);
+
+//3
+//var altura1;
+//var altura2;
+
+//altura1 = prompt("ingrese su altura en centimetros");
+//altura2 = prompt("ingrese su altura en centimetros");
+//suma = frontInt(altura1) + frontInt(altura2);
+
+//document.write("la suma de las alturas es:"  +  suma);
+
+//4
+
+///var altura = 10;
+//var base= 20;
+
+//var perimetro = altura*2 + base*2;
+
+//var superficie= base * altura;
+
+//document.write("el perimetro del rectangulo es " + perimetro);
+//document.write("<br><br>");
+//document.write("la superficie de un rectangulo es " + superficie);
+
+//4
+
+// var altura;
+//var base;
+// altura =prompt("Ingrese la altura");
+//base= prompt("ingrese la base");
+
+//var perimetro = parseInt(altura)*2 + parseInt(base)*2;
+//var superficie = parseInt(base) * parseInt(altura);
+
+//document.write("el perimetro del rectagulo es: " + perimetro);
+//document.write("<br><br>");
+//document.write("la superficie del rectangulo es:" + superficie);
+
+
+//5
+//var num1;
+//var num2;
+//num1 = prompt("ingrese un numero");
+//num2 = prompt("ingrese otro numero");
+
+//var suma = parseInt(num1) + parseInt(num2);
+//var multiplicacion = parseInt(num1) * parseInt(num2);
+
+//document.write("la suma de los numeros:" + suma);
+//document.write("<br><br>")
+//document.write("la multiplicacion de los numeros es:" + multiplicacion);
+
+
+//6
+//var numero2;
+
+
+//var numero1 = prompt("Ingresa el primer número entero: ");
+//numero2 = prompt("Ingresa el segundo número entero: ");
+//var suma;
+//var producto;
+//document.write("Calcular la suma y el producto")77suma = numero1 + numero2
+//producto = numero1 * numero2
+
+//document.write("imprimir la suma y el producto")
+//prompt ("La suma de numero1 y numero2 es: suma");
+//prompt ("El producto de numero1 y numero2 es: producto");
+
+
+//7
+//var num1;
+//var num2;
+//var num3;
+//var num4;
+//var suma;
+//var promedio;
+
+//num1 = prompt("ingrese un numero entero");
+//num2 = prompt("ingrese otro numero entero");
+//num3 = prompt("ingrese otro numero entero");
+//num4 = prompt("ingrese un ultimo numero entero ");
+//suma = parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4);
+//promedio = suma /4
+
+//document.write("el resultado de la suma es:"  + suma);
+//document.write ("<br>");
+//document.write("el resultado del promedio es:"  + promedio);
+
+
+
+
+//8
+//(var sueldo = parseFloat(prompt("Ingresa su sueldo:"));
+//if (sueldo > 300000) {
+    //document.write("Debe abonar impuesto a las ganancias.");
+// else {
+    //document.write("No debe abonar impuesto a las ganancias.");
+
+
+//9
+//var numero1 = parseInt(prompt("Ingresa el primer número entero:"));
+//var numero2 = parseInt(prompt("Ingresa el segundo número entero:"));
+
+//if (numero1 > numero2) {
+    //document.write("El mayor número es: " + numero1);
+// else if (numero2 > numero1) {
+   // document.write("El mayor número es: " + numero2);// else {
+    //document.write("Los números son iguales.");
+
+//10
+//var numero1 = parseInt(prompt("Ingresa el primer número entero:"));
+//var numero2 = parseInt(prompt("Ingresa el segundo número entero:"));
+
+//if (numero1 < numero2) {
+    
+   // var suma = numero1 + numero2;
+    //var resta = numero2 - numero1;
+
+    //document.write("Suma:", suma);
+   // document.write("<br>");
+   // document.write("Resta:", resta);
+ //else {
+
+// var producto = numero1 * numero2;
+//var division = numero1 / numero2;
+
+   // document.write("Producto:", producto);
+    //document.write("<br>");
+    //document.write("División:", division);
+
+//11
+
+/*
+var nota1 = parseFloat(prompt("Ingresa la primera nota:"));
+var nota2 = parseFloat(prompt("Ingresa la segunda nota:"));
+var nota3 = parseFloat(prompt("Ingresa la tercera nota:"));
+
+function promedio(nota1,nota2,nota3){
+    var promedio = nota1 + nota2 + nota3 / 3;
+
+    if (promedio >= 7) {
+        document.write("Promocionado");
+    }
+    else {
+       document.write("No promocionado");
+    
+    }
+}
+
+promedio(nota1,nota2,nota3);
+
+*/
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
